3d841a719f65fe7e539e4e32bb665b1814d4e797
1three <1soelkim@gmail.com>
2023-12-20 12:39:38 +0900

    [Silver IV] Title: 동전 0, Time: 168 ms, Memory: 23788 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.kt
@@ -0,0 +1,24 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    
+    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val coins = List(n) { br.readLine().toInt() }
+    
+    // 큰 수 순서대로 정렬
+    val sortedCoins = coins.sortedDescending()
+    
+    // 결과 : 동전 개수
+    var coinCnt = 0
+    var remain = k
+    for (coin in sortedCoins) {
+        coinCnt += remain / coin
+        remain %= coin
+    }
+    
+    println(coinCnt)
+    
+    br.close()
+}
