5147ae223dd20d275c7d74dc38282aed8d4cb803
1three <1soelkim@gmail.com>
2023-12-21 15:29:57 +0900

    [Silver I] Title: 회의실 배정, Time: 992 ms, Memory: 73664 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))    
+    
+    val n = br.readLine().toInt()
+    
+    val meetings = mutableListOf<Pair<Int, Int>>()
+    repeat(n) {
+        val (start, end) = br.readLine().split(" ").map { it.toInt() }
+        meetings.add(start to end)
+    }
+
+    val sortedMeetings = meetings.sortedBy { it.first }.sortedBy { it.second }
+    
+    // 결과 : maxMeetingCnt, 최대 회의의 수
+    var maxMeetingCnt = 0
+    var endTime = 0
+    
+    for (meeting in sortedMeetings) {
+        if (meeting.first >= endTime) {
+            endTime = meeting.second
+            maxMeetingCnt++
+        }
+    }
+    
+    println(maxMeetingCnt)
+    br.close()
+}
