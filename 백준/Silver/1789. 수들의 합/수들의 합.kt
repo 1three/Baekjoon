2dbaa83d537c52b2ccfe7e3c6f71fe207b9cb966
1three <1soelkim@gmail.com>
2023-12-21 16:09:37 +0900

    [Silver V] Title: 수들의 합, Time: 88 ms, Memory: 12236 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.kt
@@ -0,0 +1,24 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))    
+    val s = br.readLine().toLong()
+
+    // 조건식
+    // n * (n+1) / 2 <= s인 n
+    
+    val standard = s * 2
+    var n = Math.floor(Math.sqrt(standard.toDouble())).toLong()
+    
+    while (true) {
+        val multiply = n * (n + 1)
+        if (multiply <= standard) {
+            break
+        }
+        n--
+    }
+    
+    println(n)
+    br.close()
+}
