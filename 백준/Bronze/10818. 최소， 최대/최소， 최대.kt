60794db48678ca77f1018a5db791d44f2d8b5f17
1three <1soelkim@gmail.com>
2023-12-26 20:13:15 +0900

    [Bronze III] Title: 최소, 최대, Time: 540 ms, Memory: 133836 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -1,20 +1,15 @@
 package com.three.baekjoon
 
 fun main() = System.`in`.bufferedReader().use { br ->
-    val n = br.readLine().toInt()
-    val numbers = br.readLine().split(" ").map { it.toInt() }
-
-    var min = numbers[0]
-    var max = numbers[0]
-
-    for (i in 1 until n) {
-        val num = numbers[i]
-        if (num < min) {
-            min = num
-        }
-        if (num > max) {
-            max = num
-        }
+    br.readLine().toInt() // n 미사용
+    
+    var min = Int.MAX_VALUE
+    var max = Int.MIN_VALUE
+    
+    br.readLine().split(" ").forEach { numStr ->
+        val num = numStr.toInt()
+        if (num < min) min = num
+        if (num > max) max = num
     }
 
     println("$min $max")
