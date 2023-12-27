af93e80879993dfb00bf26c6a65175427e2fc927
1three <1soelkim@gmail.com>
2023-12-27 15:37:17 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 15224 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.kt
@@ -0,0 +1,24 @@
+package com.three.baekjoon
+
+data class Person (
+    val weight: Int,
+    val height: Int
+)
+
+fun main() = System.`in`.bufferedReader().use { br ->
+    var word = br.readLine()
+    val croatiaArray = arrayOf("c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=")
+    var result = 0
+
+    for (croatia in croatiaArray) {
+        while (true) {
+            val isFind = word.indexOf(croatia)
+            if (isFind == -1) break
+            word = word.replace(croatia, " ")
+
+        }
+    }
+
+    result = word.length
+    println(result)
+}
