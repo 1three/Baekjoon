02e9c9bcdea0ccecf9c2c4396ca86058357017b0
1three <1soelkim@gmail.com>
2024-01-03 14:01:17 +0900

    [Bronze II] Title: 음계, Time: 92 ms, Memory: 12212 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.kt
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -1,9 +1,11 @@
 fun main() = System.`in`.bufferedReader().use { br ->
-    val scales = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    val asc = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
-    val des = intArrayOf(8, 7, 6, 5, 4, 3, 2, 1)
+    val scales = br.readLine()
+    val asc = "1 2 3 4 5 6 7 8"
+    val des = "8 7 6 5 4 3 2 1"
 
-    if (asc.contentEquals(scales)) println("ascending")
-    else if (des.contentEquals(scales)) println("descending")
-    else println("mixed")
+    when (scales) {
+        asc -> println("ascending")
+        des -> println("descending")
+        else -> println("mixed")
+    }
 }
