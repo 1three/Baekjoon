53465eb3937ebf4cbf2159dc63231bd69fbc7480
1three <1soelkim@gmail.com>
2024-01-06 17:45:20 +0900

    [Silver V] Title: 나이순 정렬, Time: 444 ms, Memory: 66296 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
@@ -1,6 +1,8 @@
 fun main() = System.`in`.bufferedReader().use { br ->
     val n = br.readLine().toInt()
     val array = Array(201) { StringBuilder() }
+    val result = java.lang.StringBuilder()
+
     repeat(n) {
         val string = br.readLine()
         val (age, _) = string.split(" ")
@@ -8,5 +10,6 @@ fun main() = System.`in`.bufferedReader().use { br ->
         array[age.toInt()].append("$string\n")
     }
 
-    array.forEach { print(it) }
+    array.forEach { result.append(it) }
+    println(result)
 }
