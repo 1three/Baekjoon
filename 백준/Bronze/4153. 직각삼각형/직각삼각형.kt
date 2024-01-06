207d92a0a03e1095f351235b46b46deda63fcf78
1three <1soelkim@gmail.com>
2024-01-06 17:21:28 +0900

    [Bronze III] Title: 직각삼각형, Time: 132 ms, Memory: 20660 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.kt
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.kt
@@ -1,16 +1,12 @@
-import java.lang.StringBuilder
-
 fun main() = System.`in`.bufferedReader().use { br ->
-    val result = StringBuilder()
-
     while (true) {
-        val list = br.readLine().split(" ").map { it.toInt() }.sorted()
+        val list = br.readLine().split(" ").map { it.toInt() }
         if (list[0] == 0) break
-        val isRightTriangle = list[2] * list[2] == list[0] * list[0] + list[1] * list[1]
-
-        if (isRightTriangle) result.append("right\n")
-        else result.append("wrong\n")
+        when {
+            list[2] * list[2] == list[0] * list[0] + list[1] * list[1] -> println("right")
+            list[1] * list[1] == list[0] * list[0] + list[2] * list[2] -> println("right")
+            list[0] * list[0] == list[1] * list[1] + list[2] * list[2] -> println("right")
+            else -> println("wrong")
+        }
     }
-    
-    println(result)
 }
