17b30cb306fac959742c8595bdbe49f7f47fc750
1three <1soelkim@gmail.com>
2024-01-06 16:58:55 +0900

    [Bronze II] Title: 블랙잭, Time: 144 ms, Memory: 20760 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.kt
+++ 백준/Bronze/2798. 블랙잭/블랙잭.kt
@@ -1,33 +1,20 @@
 fun main() = System.`in`.bufferedReader().use { br ->
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val list = br.readLine().split(" ").map { it.toInt() }.toMutableList()
+    val cards = br.readLine().split(" ").map { it.toInt() }
+    var maxSum = 0
 
-    // m과 가장 차이가 적은 값 (m - sum, 0에 가까울 수록 차이가 적은 값)
-    var minDiff = 300_000
-    var result = 0
-
-    // 부분 집합의 첫 번째 값 : i
     for (i in 0..<n - 2) {
-        val iNum = list[i]
-
-        // 부분 집합의 두 번째 값 : J
+        val iNum = cards[i]
         for (j in i + 1..<n - 1) {
-            val jNum = list[j]
-
-            // 부분 집합의 세 번째 값 : k
+            val jNum = cards[j]
             for (k in j + 1..<n) {
-                val kNum = list[k]
-
-                val sum = iNum + jNum + kNum
-                val diff = m - sum
-
-                if (diff in 0..<minDiff) {
-                    minDiff = diff
-                    result = sum
+                val sum = iNum + jNum + cards[k]
+                if (sum in (maxSum + 1)..m) {
+                    maxSum = sum
                 }
             }
         }
     }
 
-    println(result)
+    println(maxSum)
 }
