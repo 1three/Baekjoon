1c6ca3b8c0be4b8670716bef12f9cf266d1985f6
1three <1soelkim@gmail.com>
2024-01-31 13:00:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2404 ms, Memory: 251092 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
@@ -0,0 +1,24 @@
+import java.lang.StringBuilder
+
+fun main() = System.`in`.bufferedReader().use { br ->
+    val n = br.readLine().toInt()
+    val cards = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val m = br.readLine().toInt()
+    val numbers = br.readLine().split(" ").map { it.toInt() }
+
+    val map = HashMap<Int, Int>()
+    val result = StringBuilder()
+
+    for (i in 0 until n) {
+        val key = cards[i]
+        map[key] = map.getOrDefault(key, 0) + 1
+    }
+
+    for (j in 0 until m) {
+        val key = numbers[j]
+        val count = map.getOrDefault(key, 0)
+        result.append(count).append(" ")
+    }
+
+    println(result)
+}
